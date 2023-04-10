2d8ef2b4fd4aec73ce8af4bbf5440ef2626e2134
Erik Silva Maia <erikm384@gmail.com>
2023-04-10 10:43:35 -0300

    feat: add MesaModel to context

--- GerenRest.RazorPages/Data/AppDbContext.cs
+++ GerenRest.RazorPages/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
 
+using GerenRest.RazorPages.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GerenRest.RazorPages.Data {
     public class AppDbContext : DbContext
     {
-
+        public DbSet<MesaModel>? MesaModel;
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("DataSource=tds.db;Cache=Shared");
 
